55c73dbd24b216762fe163a30c1d7b99d7b946e0
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-11-23 21:14:54 -0500

    doc: added documentation for celsius to fahrenheit

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9 / 5) + 32
+ """
+    Convert a temperature from Celsius to Fahrenheit.
 
+    Args:
+        celsius (float): The temperature in Celsius.
+
+    Returns:
+        float: The temperature in Fahrenheit.
+    """
 
 
 def test_should_return_celsius_to_fahrenheit():
