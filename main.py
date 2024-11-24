48e37afa0edb97a2208a089b89af43f9efbd6f82
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-11-23 21:11:35 -0500

    feat: implemented function for celsius to fahrenheit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-if __name__ == "__main__":
-    def test_should_return_celsius_to_fahrenheit():
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+
+
+
+def test_should_return_celsius_to_fahrenheit():
     celsius = 25
     fahrenheit = celsius_to_fahrenheit(celsius)
-    print(f"{celsius}°C is {fahrenheit}°F")
-    assert fahrenheit == 77, "Test case for celsius_to_fahrenheit failed!"
+    assert fahrenheit == 77 
+
+
